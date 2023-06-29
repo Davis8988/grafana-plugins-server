cd23061055cfdc1f0491c7a2698ca18fd5c11eb4
davismarsel@gmail.com <davismarsel@gmail.com>
2023-06-29 17:59:16 +0300

    Added stop_stack.sh script

---
+++ stop_stack.sh
@@ -0,0 +1,12 @@
+
+compose_file=$1
+cmndStr=""
+# Check if compose plugin is installed
+if docker compose version >/dev/null 2>&1; then cmndStr="docker compose"
+else cmndStr="docker-compose"; fi
+
+if [ ! -z "${compose_file}" ]; then cmndStr="${cmndStr} -f \"${compose_file}\""; fi
+cmndStr="${cmndStr} down"
+
+echo "Executing: ${cmndStr}"
+eval $cmndStr
