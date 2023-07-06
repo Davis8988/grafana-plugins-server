ea3d9d57666af3bae1f851b056625b133fe79496
David Yair <Davismarsel@gmail.com>
2023-07-06 15:31:02 +0300

    grafana_plugin_class.validate_grafana_plugin_class_obj(grafana_plugin_obj)

--- modules/helpers.py
+++ modules/helpers.py
@@ -234,10 +234,7 @@ def read_plugin_details_from_plugin_json_file(plugin_json_file_path):
     logging.info("Parsing json data into a GrafanaPlugin class obj")
     
     grafana_plugin_obj = grafana_plugin_class.GrafanaPlugin(**json_data) # Create GrafanaPlugin instance
-    if not grafana_plugin_class.validate_grafana_plugin_class_obj(grafana_plugin_obj):
-        err_msg = f"Validate of grafana plugin class obj failed"
-        logging.error(err_msg)
-        raise Exception(err_msg)
+    grafana_plugin_class.validate_grafana_plugin_class_obj(grafana_plugin_obj)
     return grafana_plugin
     
     
