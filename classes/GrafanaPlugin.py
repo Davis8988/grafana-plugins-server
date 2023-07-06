0c662e37ec9b9e33a55eda72dfb8e450867a1e17
David Yair <Davismarsel@gmail.com>
2023-07-06 15:15:05 +0300

    class GrafanaPlugin:

--- classes/GrafanaPlugin.py
+++ classes/GrafanaPlugin.py
@@ -1,4 +1,10 @@
-class GrafanaPlugin():
+
+
+class GrafanaPlugin:
     def __init__(self, **kwargs):
-        self.name = kwargs['name']
-        self.version = kwargs['version']
+        self.name         = kwargs.get('name', None)
+        self.type         = kwargs.get('type', None)
+        self.id           = kwargs.get('id', None)
+        self.description  = kwargs.get('description', None)
+        self.version      = kwargs.get('version', None)
+        self.dependencies = kwargs.get('dependencies', None)
