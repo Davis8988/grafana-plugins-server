86522119d349615327a20c18769a8dcf762a0a7e
David Yair <david.yair@elbitsystems.com>
2023-07-09 20:29:58 +0300

    flash(error_message, 'error')

--- modules/grafana_plugin_file.py
+++ modules/grafana_plugin_file.py
@@ -8,7 +8,7 @@ def validate_uploaded_zip_file(zip_file_obj):
     if not zip_file_obj or not helpers.allowed_file(zip_file_obj.filename):
         error_message = f'Error uploading: "{zip_file_obj.filename}" - Invalid file or file type not allowed. Only "*.zip" files are allowed'
         logging.error(error_message)
-        flash(error_message)
+        flash(error_message, 'error')
         return False
     return True
 
@@ -16,6 +16,6 @@ def validate_temp_save_zip_file(zip_file_path):
     if not file_exists(zip_file_path):
         error_message = f'Missing or unreachable uploaded zip file: {zip_file_path} - cannot validate it'
         logging.error(error_message)
-        flash(error_message)
+        flash(error_message, 'error')
         return False
     return True
