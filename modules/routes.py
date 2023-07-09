0a3c16b07f031c365496cb4062e1125c83aff3b7
David Yair <david.yair@elbitsystems.com>
2023-07-09 20:26:41 +0300

    return redirect(url_for('index'))

--- modules/routes.py
+++ modules/routes.py
@@ -96,7 +96,7 @@ def download_file(path):
 def upload():
     uploaded_file = request.files['file']
     if not grafana_plugin_file.validate_uploaded_zip_file(uploaded_file):
-        return
+        return redirect(url_for('index'))
 
     # Save the uploaded file into a temp dir first..
     extract_file_name           = uploaded_file.filename
