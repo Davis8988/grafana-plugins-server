4442e6f88c5f019dbf6ca90041920f912d2aac04
David Yair <david.yair@elbitsystems.com>
2023-07-11 08:33:01 +0300

    Added dockerfile build script printings

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,3 +1,7 @@
- 
-echo docker build -t grafana-plugins-server:0.0.1 .
-docker build -t grafana-plugins-server:0.0.1 .
+
+DOCKER_IMAGE_TAG=${$1:-grafana-plugins-server:0.0.1}
+cmndStr="docker build -t \"${DOCKER_IMAGE_TAG}\" ."
+echo "${cmndStr}"
+eval "${cmndStr}"
+if [ "$?" != "0" ]; then echo '' && echo "Error - Failure during execution of: ${cmndStr}" && echo '' && exit 1; fi
+echo "Success - Finished building docker image: ${DOCKER_IMAGE_TAG}"
