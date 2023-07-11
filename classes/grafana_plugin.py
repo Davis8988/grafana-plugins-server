524064aed845cc55d764786bbfa260e34dbb8032
David Yair <david.yair@elbitsystems.com>
2023-07-11 18:14:21 +0300

    return False

--- classes/grafana_plugin.py
+++ classes/grafana_plugin.py
@@ -22,4 +22,4 @@ def validate_grafana_plugin_class_obj(grafana_plugin):
         if attr_value is None or not attr_value.strip():
             err_msg = f"Grafana plugin class obj is missing or has an empty value attribute: '{attr_name}'"
             logging.error(err_msg)
-            raise Exception(err_msg)
+            return False
