0bec723f8ec9e6b64471bc6db12f8d47cc52ad09
David Yair <david.yair@elbitsystems.com>
2023-07-12 10:31:00 +0300

    Spaces

--- start_stack.sh
+++ start_stack.sh
@@ -1,6 +1,5 @@
 
 compose_file=$1
-echo docker run -d --name grafana-plugins-server --restart always -p 3011:3011 grafana-plugins-server:0.0.1
 cmndStr=""
 # Check if compose plugin is installed
 if docker compose version >/dev/null 2>&1; then cmndStr="docker compose"
