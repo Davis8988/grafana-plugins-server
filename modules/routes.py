12e7ac2da3693accccba77d6832ce86897ee873f
David Yair <david.yair@elbitsystems.com>
2023-07-12 08:00:37 +0300

    return render_template('login.html')

--- modules/routes.py
+++ modules/routes.py
@@ -226,11 +226,11 @@ def upload():
 # Route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
+    error_message = None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
+            error_message = 'Invalid Credentials. Please try again.'
         else:
             return redirect(url_for('index'))
     flash(error_message, 'error')
-    return render_template('login.html', error=error)
+    return render_template('login.html')
