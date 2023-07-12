4a3a361a34777482e617dc8e15fc975d6bb40c56
David Yair <david.yair@elbitsystems.com>
2023-07-12 10:57:51 +0300

    Updated version to: 1.0.0

--- run-docker-image.sh
+++ run-docker-image.sh
@@ -1,3 +1,3 @@
  
-echo docker run -d --name grafana-plugins-server --restart always -p 3011:3011 grafana-plugins-server:0.0.1
-docker run -d --name grafana-plugins-server --restart always -p 3011:3011 grafana-plugins-server:0.0.1
+echo docker run -d --name grafana-plugins-server --restart always -p 3011:3011 grafana-plugins-server:1.0.0
+docker run -d --name grafana-plugins-server --restart always -p 3011:3011 grafana-plugins-server:1.0.0
