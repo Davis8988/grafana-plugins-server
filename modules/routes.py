1bde26723cab47ac0b49bf532bac96aba3485f33
David Yair <david.yair@elbitsystems.com>
2023-07-12 09:12:34 +0300

    return render_template('login.html')

--- modules/routes.py
+++ modules/routes.py
@@ -16,6 +16,8 @@ temp_grafana_plugins_dir = runtime_config.temp_grafana_plugins_dir
 @app.route('/index', methods = ['GET', 'POST'])
 def index():
     logging.info('Accessed main dashboard page')
+    if not session.get('logged_in', False):
+        return render_template('login.html')
     grafana_plugins_repo_directory = app.config['GRAFANA_PLUGINS_REPO_DIR']
     directories = [ name for name in os.listdir(grafana_plugins_repo_directory) if os.path.isdir(os.path.join(grafana_plugins_repo_directory, name)) ]  # Get only dirs
     for x in directories:
