ff072aa9f683246a599f6bf663855f34f4401fff
David Yair <david.yair@elbitsystems.com>
2023-07-12 07:59:53 +0300

    return redirect(url_for('index'))

--- modules/routes.py
+++ modules/routes.py
@@ -221,3 +221,15 @@ def upload():
         error_message = f'Error: {str(e)}\n'
         return f"error - {str(e)}"
         
+
+
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
